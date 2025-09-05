b1cd76d4678028e69c4b6911b6ad7a28911e15f5
harundarat <harundarat@gmail.com>
2025-09-05 09:54:43 +0700

    feat: add test database setup and workout store tests

---
+++ internal/store/workout_store_test.go
@@ -0,0 +1,125 @@
+package store
+
+import (
+	"database/sql"
+	"testing"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func setupTestDB(t *testing.T) *sql.DB {
+	db, err := sql.Open("pgx", "host=localhost user=postgres password=postgres dbname=postgres port=5433 sslmode=disable")
+	if err != nil {
+		t.Fatalf("opening test db: %v", err)
+	}
+
+	err = Migrate(db, "../../migrations/")
+	if err != nil {
+		t.Fatalf("migrating test db error: %v", err)
+	}
+
+	_, err = db.Exec(`TRUNCATE workouts, workout_entries CASCADE`)
+	if err != nil {
+		t.Fatalf("truncating tabel %v", err)
+	}
+
+	return db
+}
+
+func TestCreateWorkout(t *testing.T) {
+	db := setupTestDB(t)
+	defer db.Close()
+
+	store := NewPostgresWorkoutStore(db)
+
+	tests := []struct {
+		name    string
+		workout *Workout
+		wantErr bool
+	}{
+		{
+			name: "valid workout",
+			workout: &Workout{
+				Title:           "push day",
+				Description:     "upper body",
+				DurationMinutes: 60,
+				CaloriesBurned:  200,
+				Entries: []WorkoutEntry{
+					{
+						ExerciseName: "Bench Press",
+						Sets:         3,
+						Reps:         IntPtr(10),
+						Weight:       FloatPtr(135.5),
+						Notes:        "Warm up properly",
+						OrderIndex:   1,
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "invalid workout",
+			workout: &Workout{
+				Title:           "full body",
+				Description:     "complete workout",
+				DurationMinutes: 90,
+				CaloriesBurned:  500,
+				Entries: []WorkoutEntry{
+					{
+						ExerciseName: "Plank",
+						Sets:         3,
+						Reps:         IntPtr(60),
+						Notes:        "keep form",
+						OrderIndex:   1,
+					},
+					{
+						ExerciseName:    "Squats",
+						Sets:            4,
+						Reps:            IntPtr(20),
+						DurationSeconds: IntPtr(60),
+						Weight:          FloatPtr(135.5),
+						Notes:           "full depth",
+						OrderIndex:      1,
+					},
+				},
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			createdWorkout, err := store.CreateWorkout(tt.workout)
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+
+			require.NoError(t, err)
+			assert.Equal(t, tt.workout.Title, createdWorkout.Title)
+
+			retrieved, err := store.GetWorkoutByID(int64(createdWorkout.ID))
+			assert.NoError(t, err)
+
+			assert.Equal(t, createdWorkout.ID, retrieved.ID)
+			assert.Equal(t, len(createdWorkout.Entries), len(retrieved.Entries))
+
+			for i, _ := range retrieved.Entries {
+				assert.Equal(t, tt.workout.Entries[i].ExerciseName, retrieved.Entries[i].ExerciseName)
+				assert.Equal(t, tt.workout.Entries[i].Sets, retrieved.Entries[i].Sets)
+				assert.Equal(t, tt.workout.Entries[i].OrderIndex, retrieved.Entries[i].OrderIndex)
+			}
+		})
+
+	}
+}
+
+func IntPtr(i int) *int {
+	return &i
+}
+
+func FloatPtr(i float64) *float64 {
+	return &i
+}
